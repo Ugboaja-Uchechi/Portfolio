22950b8f0553ba0a0372bc4147f8f4d270c40a1c
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-09-06 17:59:09 +0100

    create mobile menu

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+const icon = document.querySelector('.icon');
+const bar = document.querySelector('.menu-bar');
+const closeIcon = document.querySelector('.x-icon');
+const container = document.querySelector('.mobile-header-nav-links');
+const portfolio = document.querySelector('[portfolio-section]');
+const about = document.querySelector('[about-section]');
+const contact = document.querySelector('[contact-section]');
 
-  gtag('config', 'G-5D3J650GZE');
+function openBar() {
+  container.style.display = 'flex';
+}
+
+function close() {
+  container.style.display = 'none';
+}
+
+// icon.addEventListener('click', openBar)
+// closeIcon.addEventListener('click', close)
+// portfolio.addEventListener('click', close)
+// about.addEventListener('click', close)
+// contact.addEventListener('click', close)
